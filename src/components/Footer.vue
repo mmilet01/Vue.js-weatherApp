2666a0d97a0f2ad74e8047deabda9311e9d22862
Karlo Mileta <mmilet01@fesb.hr>
2019-05-29 17:33:28 +0200

    footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,17 @@
 <template>
   <footer>
-    <h1>Footer to be done</h1>
+    <div class="subscribe">
+      <h1>Subscribe to WeatherApp</h1>
+      <input type="text" placeholder="Enter e-mail">
+    </div>
+    <div class="follow">
+      <h2>Follow us</h2>
+      <div class="icons">
+        <i class="fab fa-twitter fa-2x"/>
+        <i class="fab fa-facebook-square fa-2x"/>
+        <i class="fab fa-instagram fa-2x"/>
+      </div>
+    </div>
   </footer>
 </template>
 
@@ -8,7 +19,28 @@
 <style scoped>
 footer {
   margin-top: 15px;
-  height: 100px;
+  height: 150px;
   background-color: rgb(36, 163, 206);
+  display: flex;
+  justify-content: space-around;
+}
+input {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  background: none;
+  border: none;
+  font-size: 26px;
+  border-bottom: 2px solid darkblue;
+  color: darkblue;
+  margin-bottom: 15px;
+}
+.follow {
+  display: flex;
+  flex-direction: column;
+}
+i {
+  width: 50px;
+  height: 30px;
+  margin: 7px;
+  cursor: pointer;
 }
 </style>
